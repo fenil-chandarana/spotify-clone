65bebc4d2e179c2a4f564cb853d3e6f906bb7fba
fenil-chandarana <fenilchandarana01@gmail.com>
2023-07-29 19:08:15 +0530

    final

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images:{
+        domains:[
+            "shjxrcculzbflxlgxtox.supabase.co"
+        ]
+    }
+}
+
+module.exports = nextConfig
